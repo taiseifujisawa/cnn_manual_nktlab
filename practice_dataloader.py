b1aa875d60d9e505a0ffa862d04ffd65e45b11c6
Taisei <taisei.mrchil@gmail.com>
2023-10-16 13:02:23 +0900

    コメントアウト

--- practice_dataloader.py
+++ practice_dataloader.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     print(f"type(train_loader) = {type(train_loader)}")                 # 型
     first_batch = next(iter(train_loader))                              # イテレータにしてnextで最初のミニバッチを取り出す
     
-    print(f"first_batch[0] = {first_batch[0]}")                         # 各ミニバッチは2つの要素を持つリストで、0番目がデータ
-    print(f"first_batch[1] = {first_batch[1]}")                         # 1番目が教師ラベルのリスト
+    # print(f"first_batch[0] = {first_batch[0]}")                         # 各ミニバッチは2つの要素を持つリストで、0番目がデータ
+    # print(f"first_batch[1] = {first_batch[1]}")                         # 1番目が教師ラベルのリスト
     print(f"type(first_batch[0]) = {type(first_batch[0])}")             # 自動でtensorになる
     print(f"type(first_batch[1]) = {type(first_batch[1])}")             
     print(f"first_batch[0].shape = {first_batch[0].shape}")             # (100, 8) <-(batchsize, 説明変数数)
